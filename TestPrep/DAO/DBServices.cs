ab9d6edc9aad698b10a546faa321d3d14d924647
ZzzGrusha <s22228@pjwstk.edu.pl>
2022-04-21 12:07:36 +0200

    done2

--- TestPrep/DAO/DBServices.cs
+++ TestPrep/DAO/DBServices.cs
@@ -2,192 +2,194 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Tutorial3.Models;
 
 namespace Tutorial3.DAO
 {
     public class DBServices : IDBServices
     {
-        public static T ConvertFromDBVal<T>(object obj)
+        private IConfiguration _configuration;
+
+        public DBServices(IConfiguration configuration)
         {
-            if (obj == null || obj == DBNull.Value)
-            {
-                return default(T);
-            }
-            else
-            {
-                return (T)obj;
-            }
+            _configuration = configuration;
         }
+
+
+        //
         
-        public bool CheckIfIdExist(int id)
+        public List<int> ReturnFlightCodes(int id)
         {
-            
-            int tmp_res = 0;
-            using (var con = new SqlConnection("Data Source = db-mssql.pjwstk.edu.pl; Initial Catalog=2019SBD; Integrated Security = True"))
+            List<int> Flights = new List<int>();
+            using (var con = new SqlConnection(_configuration.GetConnectionString("ProductionDb")))
             using(var com = new SqlCommand())
             {
                 com.Connection = con;
-                com.CommandText = "Select Count(1) as counter from  FireTruck as ft Where ft.IdFiretruck = @id";
+                com.CommandText = "Select *  from  Flight_Passenger as fp Where fp.IdPassenger = @id";
                 com.Parameters.AddWithValue("id", id);
                 con.Open();
                 var dr = com.ExecuteReader();
                 while (dr.Read())
                 {
-                    tmp_res = (int) dr["counter"];
+                    if((int)dr["IdFlight"]>0)Flights.Add((int)dr["IdFlight"]);
                 }
 
-                return tmp_res >= 1;
+                return Flights;
             }
         }
 
-        public Truck ReturnTheTruck(int id)
+        public Plane CreatePlane(int id, string name, int maxSeats)
         {
-            int tmp_res = 0;
-            using (var con = new SqlConnection("Data Source = db-mssql.pjwstk.edu.pl; Initial Catalog=2019SBD; Integrated Security = True"))
-            using(var com = new SqlCommand())
+            Plane planeRes = new Plane
             {
-                com.Connection = con;
-                com.CommandText = "Select *  from  FireTruck as ft Where ft.IdFiretruck = @id";
-                com.Parameters.AddWithValue("id", id);
-                con.Open();
-                var dr = com.ExecuteReader();
-                Truck truck = null;
-                while (dr.Read())
-                {
-                     truck = new Truck
-                    {
-                        IDTuck = (int) dr["IdFireTruck"],
-                        OperationNumber = (string)dr["OperationNumber"],
-                        SpecialEquipment = (bool)dr["SpecialEquipment"],
-                        Actions = ReturnActions(id)
-                    };
-                }
-
-                return truck;
-            }
+                IDPlane = id,
+                Name = name,
+                MaxSeats = maxSeats
+            };
+            return planeRes;
         }
 
-        public List<Action> ReturnActions(int id)
+        public IEnumerable<Flight> ReturnFlightsPassenger(int id)
         {
-            List<int> ActionCodes = ReturnActionCodes(id);
-            List<Action> res = new List<Action>();
-            for (int i = 0; i < ActionCodes.Count; i++)
+            List<int> actionCodes = ReturnFlightCodes(id);
+            List<Flight> flights = new List<Flight>();
+            for (int i = 0; i < actionCodes.Count; i++)
             {
-                using (var con = new SqlConnection("Data Source = db-mssql.pjwstk.edu.pl; Initial Catalog=2019SBD; Integrated Security = True"))
+                using (var con = new SqlConnection(_configuration.GetConnectionString("ProductionDb")))
                 using(var com = new SqlCommand())
                 {
-                    //Finding number of firefighters
-                    int FireFighetrsCounter = 0;
                     com.Connection = con;
-                    com.CommandText = "Select Count(1) as counter  from  FireFighter_Action as fa Where fa.idAction = @id";
-                    com.Parameters.AddWithValue("id", ActionCodes[i]);
+                    com.CommandText = @"Select c.City as city, p.idPlane as plane, p.Name as name, p.MaxSeats as seats, f.FlightDate
+                                        From Flight as f, Plane as p, CityDict as c
+                                        Where f.IdCityDict = c.IdCityDict and p.idPlane = f.idPlane and 
+                                        f.IdFlight = @id";
+                    com.Parameters.AddWithValue("id", actionCodes[i]);
                     con.Open();
+                    Plane plane = new Plane();
+                    string city ="";
                     var dr = com.ExecuteReader();
                     while (dr.Read())
                     {
-                        FireFighetrsCounter = (int) dr["counter"];
+                        city = dr["city"].ToString();
+                        int idPlane = (int)dr["plane"];
+                        string name = dr["name"].ToString();
+                        int maxSeats = (int)dr["seats"];
+                        plane = CreatePlane(idPlane, name, maxSeats);
                     }
-                    dr.Close();
-                    con.Close();
-                    //Finding assigment date
-                    DateTime AssDate = new DateTime();
-                    com.CommandText = "Select AssigmentDate as ad  from  FireTruck_Action as fa Where fa.idAction = @id3 and fa.IdFireTruck = @id2";
-                    com.Parameters.AddWithValue("id3", ActionCodes[i]);
-                    com.Parameters.AddWithValue("id2", id);
-                    con.Open();
-                    dr = com.ExecuteReader();
-                    while (dr.Read())
+
+                    Flight flight = new Flight
                     {
-                        AssDate = (DateTime) dr["ad"];
-                    }
-                    dr.Close();
-                    con.Close();
-                    //Creating Action
-                    Action action = new Action();
-                    com.CommandText = "Select *  from  Action as a Where a.idAction = @id4";
-                    com.Parameters.AddWithValue("id4", ActionCodes[i]);
-                    con.Open();
-                    dr = com.ExecuteReader();
-                    while (dr.Read())
-                    {
-                        action = new Action
-                        {
-                            StartTime = dr["StartTime"] is DateTime ? (DateTime) dr["StartTime"] : default,
-                            EndTime = dr["EndTime"] is DateTime ? (DateTime) dr["EndTime"] : default,
-                            AssignedTime = AssDate,
-                            NumOfFirefight = FireFighetrsCounter
-                        };
-                    }
-                    res.Add(action);
-                    dr.Close();
-                    con.Close();
+                        Plane = plane,
+                        CityName = city
+                    };
+                    flights.Add(flight);
                 }
             }
-
-            return res;
+            return flights;
         }
 
-        public List<int> ReturnActionCodes(int id)
+        public bool CheckPassengerExists(int id)
         {
-            List<int> res = new List<int>();
-            using (var con = new SqlConnection("Data Source = db-mssql.pjwstk.edu.pl; Initial Catalog=2019SBD; Integrated Security = True"))
+            int passengerCounter = 0;
+            using (var con = new SqlConnection(_configuration.GetConnectionString("ProductionDb")))
             using(var com = new SqlCommand())
             {
                 com.Connection = con;
-                com.CommandText = "Select *  from  FireTruck_Action as fa Where fa.IdFiretruck = @id";
-                com.Parameters.AddWithValue("id", id);
-                con.Open();
-                var dr = com.ExecuteReader();
-                Truck truck = null;
-                while (dr.Read())
-                {
-                    if((int)dr["idAction"]>0)res.Add((int)dr["idAction"]);
-                }
-
-                return res;
-            }
-        }
-
-        public bool CheckDateConstraints(DateTime date, int id)
-        {
-            bool Res1 = false;
-            bool Res2 = false;
-            using (var con = new SqlConnection("Data Source = db-mssql.pjwstk.edu.pl; Initial Catalog=2019SBD; Integrated Security = True"))
-            using(var com = new SqlCommand())
-            {
-                com.Connection = con;
-                com.CommandText = "Select *  from  Action as a Where a.IdAction = @id";
+                com.CommandText = "Select Count(1) as counter from  Passenger as p Where p.idPassenger = @id";
                 com.Parameters.AddWithValue("id", id);
                 con.Open();
                 var dr = com.ExecuteReader();
                 while (dr.Read())
                 {
-                    if((DateTime)dr["StartTime"]<date)Res1=true;
-                    DateTime EndTime = dr["EndTime"] is DateTime ? (DateTime) dr["EndTime"] : default;
-                    if(EndTime==DateTime.MinValue)Res1=true;
+                    passengerCounter = (int) dr["counter"];
                 }
 
-                return Res1&&Res2;
+                return passengerCounter >= 1;
             }
         }
 
-        public void UpdateDate(DateTime date, int id)
+        public bool WasFlightFinished(int id)
         {
-            using (var con = new SqlConnection("Data Source = db-mssql.pjwstk.edu.pl; Initial Catalog=2019SBD; Integrated Security = True"))
+            bool wasFinished = false;
+            using (var con = new SqlConnection(_configuration.GetConnectionString("ProductionDb")))
+            using(var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "Select FlightDate as dateF from  Flight as f Where f.idFlight = @id";
+                com.Parameters.AddWithValue("id", id);
+                con.Open();
+                DateTime? dateTime = null;
+                var dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    dateTime = (DateTime) dr["dateF"];
+                }
+
+                if (dateTime<DateTime.Now)
+                {
+                    wasFinished = true;
+                }
+
+                return wasFinished;
+            }
+        }
+
+        public int SeatsPerFlight(int id)
+        {
+            int seatsPerFlight = 0;
+            using (var con = new SqlConnection(_configuration.GetConnectionString("ProductionDb")))
+            using(var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "Select maxSeats as seats from  Plane as p, Flight as f Where f.idPlane = p.IdPlane and f.IdFlight = @id";
+                com.Parameters.AddWithValue("id", id);
+                con.Open();
+                var dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    seatsPerFlight = (int) dr["seats"];
+                }
+                return seatsPerFlight;
+            }
+        }
+
+        public int PassengersPerFlight(int id)
+        {
+            int passengersPerFlight = 0;
+            using (var con = new SqlConnection(_configuration.GetConnectionString("ProductionDb")))
+            using(var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "Select Count(1) as passangers from  Flight_Passenger as fp, Flight as f Where f.idFlight = fp.idFlight and f.IdFlight = @id";
+                com.Parameters.AddWithValue("id", id);
+                con.Open();
+                var dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    passengersPerFlight = (int) dr["passangers"];
+                }
+                return passengersPerFlight;
+            }
+        }
+
+        public void InsertPassengers(int idF, int idP)
+        {
+            using (var con = new SqlConnection(_configuration.GetConnectionString("ProductionDb")))
             {
                 con.Open();
-                SqlTransaction tr = con.BeginTransaction();
-                SqlCommand command = new SqlCommand("Update Action Set EndTime = @date Where IdAction = @id",con,tr);
-                command.Parameters.AddWithValue("id", id);
-                command.Parameters.AddWithValue("date", date);
-                Console.WriteLine("UPDATED");
-                command.ExecuteNonQuery();
-                tr.Commit();
-                tr.Dispose();
-                con.Close();
+                using (SqlTransaction tr = con.BeginTransaction())
+                {
+                    SqlCommand command = new SqlCommand(@"Insert into Flight_Passenger(IdFlight, IdPassenger)values(@idF, @idP)", con,tr);
+                    command.Parameters.AddWithValue("idF", idF);
+                    command.Parameters.AddWithValue("idP", idP);
+                    command.ExecuteNonQuery();
+                    tr.Commit();
+                }
             }
         }
     }
+    
     
     
 }
--- TestPrep/DAO/IDBServices.cs
+++ TestPrep/DAO/IDBServices.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using Tutorial3.Models;
 
 namespace Tutorial3.DAO
 {
     public interface IDBServices
     {
-        public bool CheckIfIdExist(int id);
-        
-        public Truck ReturnTheTruck(int id);
-        
-        public List<Action> ReturnActions(int id);
+        public List<int> ReturnFlightCodes(int id);
 
-        public List<int> ReturnActionCodes(int id);
+        public Plane CreatePlane(int id, string name, int maxSeats);
 
-        public bool CheckDateConstraints(DateTime date, int id);
+        public IEnumerable<Flight> ReturnFlightsPassenger(int id);
 
-        public void UpdateDate(DateTime date, int id);
+        public bool CheckPassengerExists(int id);
+
+        public bool WasFlightFinished(int id);
+
+        public int SeatsPerFlight( int id);
+
+        public int PassengersPerFlight(int id);
+
+        public void InsertPassengers(int idF, int idP);
+
     }
 }
